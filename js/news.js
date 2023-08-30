979317aff83b6b682949a54597fc6cc0af1c49ca
rabby-web <rabby.webdeveloper@gmail.com>
2023-08-30 20:08:38 +0600

    news category show in nav bar

--- js/news.js
+++ js/news.js
@@ -1,1 +1,17 @@
-console.log("Hello news");
+const handleCategory = async () => {
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/news/categories"
+  );
+  const data = await response.json();
+  const newsCategory = data.data.news_category;
+  console.log(newsCategory);
+  newsCategory.forEach((category) => {
+    const ulContainer = document.getElementById("ul-list");
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <li><a>${category.category_name}</a></li>
+        `;
+    ulContainer.appendChild(li);
+  });
+};
+handleCategory();
