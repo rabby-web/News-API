98b8d07f904ae8f07c0eca2b9c7c7d44117a4b4e
rabby-web <rabby.webdeveloper@gmail.com>
2023-08-30 20:54:37 +0600

    news data load and picture daynamic add

--- js/news.js
+++ js/news.js
@@ -4,8 +4,9 @@ const handleCategory = async () => {
   );
   const data = await response.json();
   const newsCategory = data.data.news_category;
-  console.log(newsCategory);
-  newsCategory.forEach((category) => {
+  const newsItems = newsCategory.slice(2, 8);
+  //   console.log(newsCategory, newsItems);
+  newsItems.forEach((category) => {
     const ulContainer = document.getElementById("ul-list");
     const li = document.createElement("li");
     li.innerHTML = `
@@ -14,4 +15,64 @@ const handleCategory = async () => {
     ulContainer.appendChild(li);
   });
 };
+const handleLoadNews = async (categoryId) => {
+  console.log(categoryId);
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/news/category/${categoryId}`
+  );
+  const data = await response.json();
+  const newsIdCategory = data.data;
+  newsIdCategory?.forEach((news) => {
+    const cardContainer = document.getElementById("card-container");
+    const div = document.createElement("div");
+    div.classList.add = `grid grid-cols-2 gap-4 m-5 md:m-10`;
+    div.innerHTML = `
+    <div class="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="${news.image_url}"
+              alt="Shoes"
+            />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">
+              Biden Pledges Nearly $3 Billion To Ukraine
+              <div class="badge badge-secondary p-5">Excellent</div>
+            </h2>
+            <p>
+              Wednesday, August 24, 2022 | Tag Cloud Tags: Biden, EU, Euro,
+              Europe, Joe Biden,
+            </p>
+            <div class="card-footer flex justify-between mt-8">
+              <div class="flex">
+                <div>
+                  <div class="avatar online">
+                    <div class="w-14 rounded-full">
+                      <img
+                        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <h6>Jimmy Dane</h6>
+                  <small>2022-08-24 17:27:34</small>
+                </div>
+              </div>
+              <div class="card-detaild-btn">
+                <button
+                  class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900"
+                >
+                  Details
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+    `;
+    cardContainer.appendChild(div);
+  });
+  console.log(data);
+};
+handleLoadNews("01");
 handleCategory();
